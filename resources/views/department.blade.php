1622d51b7fa77ca59f5378c3f8a0f163ff4f5b6c
romaven <romaven.com@gmail.com>
2017-07-12 14:29:14 +0300

    Правки отделений

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -20,26 +20,29 @@
 
     <div class="services-page clearfix">
         <div class="container">
-            <div class="row text-center">
-                @foreach($doctors as $doctor)
-                    <div class="isotope-item cardio-clinic primary-health-care col-lg-3 col-md-4 col-sm-6">
-                        <article class="common-doctor clearfix hentry">
-                            <figure>
-                                <a href="{{ route('site.doctor', ['url' => $doctor->url]) }}" onclick="return false;"
-                                   title="{{ $doctor->full_name }}">
-                                    <img width="670" height="500" src="/images/doctors/thumb-{{ $doctor->photo }}"
-                                         class="attachment-gallery-post-single wp-post-image"
-                                         alt="{{ $doctor->full_name }}"/> </a>
-                            </figure>
-                            <div class="text-content"><br>
-                                <h5 class="entry-title"><a
-                                            href="{{ route('site.doctor', ['url' => $doctor->url]) }}">{{ $doctor->full_name }}</a>
-                                </h5>
-                            </div>
-                        </article>
-                    </div>
-                @endforeach
-            </div>
+            @if(count($doctors) > 0)
+                <div class="row text-center">
+                    @foreach($doctors as $doctor)
+                        <div class="isotope-item cardio-clinic primary-health-care col-lg-3 col-md-4 col-sm-6">
+                            <article class="common-doctor clearfix hentry">
+                                <figure>
+                                    <a href="{{ route('site.doctor', ['url' => $doctor->url]) }}"
+                                       onclick="return false;"
+                                       title="{{ $doctor->full_name }}">
+                                        <img width="670" height="500" src="/images/doctors/thumb-{{ $doctor->photo }}"
+                                             class="attachment-gallery-post-single wp-post-image"
+                                             alt="{{ $doctor->full_name }}"/> </a>
+                                </figure>
+                                <div class="text-content"><br>
+                                    <h5 class="entry-title"><a
+                                                href="{{ route('site.doctor', ['url' => $doctor->url]) }}">{{ $doctor->full_name }}</a>
+                                    </h5>
+                                </div>
+                            </article>
+                        </div>
+                    @endforeach
+                </div>
+            @endif
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <article id="post-112" class=" clearfix post-112 page type-page status-publish hentry">
